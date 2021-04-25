b4fd0e6658d2346dce26e145f62e7a9b690fe58e
Eric Rabinowitz <ericrabinowitz@me.com>
2021-04-25 10:59:49 -0400

    add ts type checker

---
+++ next.config.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+  webpack(config, options) {
+    const { dev, isServer } = options;
+
+    // Do not run type checking twice:
+    if (dev && isServer) {
+      config.plugins.push(new ForkTsCheckerWebpackPlugin());
+    }
+
+    return config;
+  },
+};
